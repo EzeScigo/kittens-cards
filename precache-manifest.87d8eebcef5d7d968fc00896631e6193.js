6b0cbbff8713f3a68b48721eafd8bb0ae69312e8
EzeScigo <scigolini@gmail.com>
2020-10-04 01:17:45 +0200

    Updates

---
+++ precache-manifest.87d8eebcef5d7d968fc00896631e6193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32866e5842b75018c8d6cc3afe9d23e9",
+    "url": "/kittens-cards/index.html"
+  },
+  {
+    "revision": "a1157810201b9b474a6e",
+    "url": "/kittens-cards/static/css/main.0cdadec1.chunk.css"
+  },
+  {
+    "revision": "a7762404c21e1a130b2a",
+    "url": "/kittens-cards/static/js/2.a4e7d546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittens-cards/static/js/2.a4e7d546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1157810201b9b474a6e",
+    "url": "/kittens-cards/static/js/main.18837e69.chunk.js"
+  },
+  {
+    "revision": "2f419d69f9537fbaffa5",
+    "url": "/kittens-cards/static/js/runtime-main.ee8a7305.js"
+  }
+]);
